194e4d0128033d298a6ec9e78e26e79b8e9d4ea7
nikcornish <nik.cornish@gmail.com>
2014-05-06 16:30:16 +0200

    added removeAllGuides script

---
+++ removeAllGuides.js
@@ -0,0 +1,29 @@
+//  removeAllGuides: Removes all guides currently applied to the active document
+
+/*
+    An adaptation of a script found in this thread: https://forums.adobe.com/message/5712262
+
+HOW IT WORKS:
+    1.  COUNT and ALERT the current number of guides
+    2.  CONFIRM and DELETES all guides
+*/
+
+
+
+
+
+
+
+// START //
+
+//  1.  GET the active document, then the number of guides
+var d = app.activeDocument;
+var guideCount = d.guides.length;
+
+//  2. CONFIRM and DELETE all guides
+var answer = confirm("You currently have " + guideCount + " guides." + "\nDelete all guides?\nCareful, there's no going back.");
+if (answer) {
+    d.guides.removeAll();
+}
+
+// END //
